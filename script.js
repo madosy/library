784e5aba4968938cc9c3a2ec6d89db90f391c52b
Dasom Heo <dasomee@outlook.com>
2023-01-28 16:51:52 -0500

    Allow modification by user when double-click on a book

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 const bookshelf = document.querySelector('div.bookshelf')
 const book = document.querySelector('.book')
 
@@ -6,14 +6,30 @@ const book = document.querySelector('.book')
 //I don't have to keep creating eventlister for individual books.
 //this is for deleting books or updating read status in myLibrary
 bookshelf.onclick = (e) => {
-    const bookid = e.target.closest('.book').dataset.bookid
+    const getBookid = () => e.target.closest('.book').dataset.bookid;
+
     if (e.target.classList.contains('x')){
+        const bookid = getBookid() //I have to call from inside to prevent errors when clicking outside of a book but inside bookshelf
         e.target.closest('.book').remove();
         deleteBookFromLibrary(bookid);
     } else if (e.target.classList.contains('checkbox')){
+        const bookid = getBookid()
         updateRead(bookid, e.target.checked)
     }
+
 }
+
+const getBook = function findBookByUIDThenReturnBook(uid) {
+   const getInd = myLibrary.map((item) => item.uid).indexOf(uid)
+   return myLibrary[getInd]
+}
+
+bookshelf.addEventListener('dblclick', (e) => {
+    const getBookid = () => e.target.closest('.book').dataset.bookid;
+    const selectedBook = getBook(getBookid())
+
+    detailedView(selectedBook);
+})
 
 const uid = function generateUniqueID() {
     return Date.now().toString(36) + Math.random().toString(36).substring(2)
@@ -49,39 +65,6 @@ const displayBook = function addLastBookToBookshelf(newBook) {
     bookshelf.append(copyBook);   
 }
 
-
-//innerHTML method was kind of dangerous especially the way I was doing it.
-// function displayBook(book, bookid) {
-//     const bookForDisplay = document.createElement('div')
-//     bookForDisplay.dataset.bookid = bookid
-//     bookForDisplay.classList.add('book')
-
-//     //may be better to get rid of the variables -> query all the child nodes -> replace the textcontent for security.
-//     bookForDisplay.innerHTML = `
-//         <div class="x"></div>
-//         <div class="title">${book.title}</div>
-//         <div class="author">${book.author}</div>
-//         <div class="pages">${book.pages}</div>
-//         <div class="checkbox">
-//             <input type="checkbox" id="book-${bookid}">
-//             <label for="book-${bookid}">Read</label>
-//         </div>`
-
-//     bookshelf.appendChild(bookForDisplay)
-    
-//     //update checkbox based on read status
-//     let createdBook = document.querySelector(`.book[data-bookid="${bookid}"`)
-//     let checkbox_div = createdBook.lastElementChild;
-//     let readStatus = myLibrary[bookid].read;
-//     checkbox_div.firstElementChild.checked = readStatus
-    
-//     //SOLVE THIS: How do I add event listener to the X and the label? 
-
-// }
-
-
-
-
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(
         title,
@@ -115,6 +98,7 @@ bookForm.addEventListener('submit', (e) => {
     
     //html form has various functions already (was clearing fields manually before.)
     bookForm.reset();
+    toggleOverlay(0);
 })
 
 function toggleOverlay(mode) {
@@ -123,44 +107,80 @@ function toggleOverlay(mode) {
     overlay.classList.remove('visible')
 }
 
+function detailedView(testBook) {
+    changeFormDisplay('modify');
+    toggleOverlay(1);
+    // const testBook = myLibrary[0];
+    bookForm.uid.value = testBook.uid;
+    bookForm.title.value = testBook.title;
+    bookForm.author.value = testBook.author;
+    bookForm.pages.value = testBook.pages;
+    bookForm.read.checked = testBook.read;
+}
 
-addBookToLibrary(
-    'Jane Eyre',
-    'Charlotte Bronte',
-    536,
-    false
-)
 
-addBookToLibrary(
-    "Harry Potter and the Sorcerer's Stone",
-    "J. K. Rowling",
-    255,
-    true
-)
+function modifyBook() {
+    if (!bookForm.checkValidity()){
+        bookForm.reportValidity();
+        return
+    }
 
-addBookToLibrary(
-    "The Alchemist",
-    "Paulo Coelho",
-    300,
-    false
-)
+    const uid = bookForm.uid.value;
+    console.log(bookForm.uid.value)
 
-addBookToLibrary(
-    "Of Mice and Men",
-    "John Steinbeck",
-    107,
-    false
-)
+    //modify data in myLibrary
+    const book = getBook(bookForm.uid.value)
+    book.title = bookForm.title.value;
+    book.author = bookForm.author.value;
+    book.pages = bookForm.pages.value;
+    book.read = bookForm.read.checked;
 
-addBookToLibrary(
-    'Harry Potter and the Sorcerer\'s Stone And Hairy Potato',
-    'Dasom',
-    234,
-    true
-)
-addBookToLibrary(
-    'My Grandmother Asked Me to Tell You She’s Sorry',
-    'FREDRIK BACKMAN',
-    250,
-    false
-)
+    //modify visually
+    const selectedBook = document.querySelector(`.book[data-bookid="${uid}"]`)
+    console.log(selectedBook);
+    selectedBook_fields = selectedBook.querySelectorAll('.field')
+    selectedBook_fields[0].textContent = book.title
+    selectedBook_fields[1].textContent = book.author
+    selectedBook_fields[2].textContent = book.pages
+    selectedBook_fields[3].checked = book.read
+
+    changeFormDisplay('reset')
+    bookForm.reset();
+    toggleOverlay(0);
+}
+
+function changeFormDisplay(mode) {
+    const modifyButton = document.querySelector('#modify-button');
+    const submitButton = document.querySelector('#submit-button')
+    const formTitle = document.querySelector('.form-container h2')
+    switch (mode) {
+        case 'reset':
+            formTitle.textContent = 'Add a new book:'
+            modifyButton.style.display = 'none'
+            submitButton.style.display = 'unset'
+            break;
+        case 'modify':
+            formTitle.textContent = 'Modify existing book:';
+            modifyButton.style.display = 'unset'
+            submitButton.style.display = 'none';
+            break;
+        default:
+            break;
+    }
+}
+
+function cancelSubmit() {
+    toggleOverlay(0);
+    changeFormDisplay('reset'); 
+    bookForm.reset();
+}
+
+//just adding data so it won't be empty;
+myLibrary.push({title: 'Jane Eyre', author: 'Charlotte Bronte', pages: 536, read: false, uid: 'ldg8h4327lr9i3bwx9v'})  
+myLibrary.push({title: 'Harry Potter and the Sorcerer\'s Stone', author: 'J. K. Rowling', pages: 255, read: true, uid: 'ldg8h432zstj72n8ymk'})
+myLibrary.push({title: 'The Alchemist', author: 'Paulo Coelho', pages: 300, read: false, uid: 'ldg8h433vc9ug34um1h'})
+myLibrary.push({title: 'Of Mice and Men', author: 'John Steinbeck', pages: 107, read: false, uid: 'ldg8h433ptxa0k944ua'})
+myLibrary.push({title: 'My Grandmother Asked Me to Tell You She’s Sorry', author: 'FREDRIK BACKMAN', pages: 250, read: false, uid: 'ldg8h4330eqvct2xsctf'})
+myLibrary.forEach((obj) => Object.setPrototypeOf(obj,Object.create(Book.prototype)));
+
+myLibrary.forEach((book) => displayBook(book));
